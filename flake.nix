9439ce02a1fc127861e458ce6ade8a853778e3e3
Sumner Evans <me@sumnerevans.com>
2024-01-09 09:20:54 -0700

    deps: templ v0.2.501 -> v0.2.513
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- flake.nix
+++ flake.nix
@@ -5,41 +5,35 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     templ = {
-      url = "github:a-h/templ/v0.2.501";
+      url = "github:a-h/templ/v0.2.513";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = { self, nixpkgs, flake-utils, templ }:
-    (flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          templ-pkg = templ.packages.${system}.templ;
-        in
-        {
-          packages = rec {
-            default = webfortune;
-            webfortune = pkgs.buildGoModule {
-              pname = "webfortune";
-              version = "unstable-2023-12-30";
-              src = self;
-              vendorHash = "sha256-o7E1UvE8pSDPc0Sq/aN50pVOS038TZRIlE3p9fjHxmo=";
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays =
+            [ (final: prev: { inherit (templ.packages.${system}) templ; }) ];
+        };
+      in {
+        packages = rec {
+          default = webfortune;
+          webfortune = pkgs.buildGoModule {
+            pname = "webfortune";
+            version = "unstable-2023-01-09";
+            src = self;
+            vendorHash = "sha256-sYdS8+XvKMxRF2OrvpP92DhObYsea0oWKzy8tKFZ8FI=";
 
-              preBuild = ''
-                ${templ-pkg}/bin/templ generate
-              '';
-            };
+            preBuild = ''
+              ${pkgs.templ}/bin/templ generate
+            '';
           };
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              go_1_21
-              gopls
-              gotools
-              pre-commit
-              templ-pkg
-            ];
-          };
-        }
-      ));
+        };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ go_1_21 gopls gotools pre-commit pkgs.templ ];
+        };
+      }));
 }
