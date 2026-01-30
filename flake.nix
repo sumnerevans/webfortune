d3d8a2350fcf64117fe8f467c97a58b09d4f8c41
Sumner Evans <me@sumnerevans.com>
2026-01-30 04:01:58 -0700

    deps: upgrade Go and Nix dependencies
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- flake.nix
+++ flake.nix
@@ -3,37 +3,57 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     templ = {
-      url = "github:a-h/templ/v0.2.778";
+      url = "github:a-h/templ/v0.3.977";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, templ }:
-    (flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays =
-            [ (final: prev: { inherit (templ.packages.${system}) templ; }) ];
-        };
-      in {
-        packages = rec {
-          default = webfortune;
-          webfortune = pkgs.buildGoModule {
-            pname = "webfortune";
-            version = "unstable-2024-10-23";
-            src = self;
-            vendorHash = "sha256-f+uSSsjNyAVT2hLUgP6CDQrh3GvoJX53/BTMUdPzVzM=";
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      templ,
+    }:
+    (flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem =
+        {
+          lib,
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ (final: prev: { inherit (templ.packages.${system}) templ; }) ];
+          };
 
-            preBuild = ''
-              ${pkgs.templ}/bin/templ generate
-            '';
+          packages = rec {
+            default = webfortune;
+            webfortune = pkgs.buildGoModule {
+              pname = "webfortune";
+              version = "unstable-2024-10-23";
+              src = self;
+              vendorHash = "sha256-fPkJYxLxw4KE+Bj4tTs2RBSb7Q+b7/nag0tYaqWkS1I=";
+
+              preBuild = ''
+                ${pkgs.templ}/bin/templ generate
+              '';
+            };
+          };
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gopls
+              gotools
+              pre-commit
+              pkgs.templ
+            ];
           };
         };
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ go gopls gotools pre-commit pkgs.templ ];
-        };
-      }));
+    });
 }
--- go.mod
+++ go.mod
@@ -1,15 +1,17 @@
 module github.com/sumnerevans/webfortune
 
-go 1.21
+go 1.24.0
+
+toolchain go1.24.12
 
 require (
-	github.com/a-h/templ v0.2.778
+	github.com/a-h/templ v0.3.977
 	github.com/mitchellh/go-wordwrap v1.0.1
-	github.com/rs/zerolog v1.33.0
+	github.com/rs/zerolog v1.34.0
 )
 
 require (
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
 )
